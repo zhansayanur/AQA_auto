5a7e44c1128fbfb6a016d56664cab2662c0d068d
Zhansaya <zhansayanur19999@gmail.com>
2024-03-02 02:03:02 +0500

    New commit

---
+++ src/main/java/compare_elements/ElementComparison.java
@@ -0,0 +1,44 @@
+package compare_elements;
+
+import org.openqa.selenium.WebElement;
+
+public class ElementComparison {
+    public static void compareElements(WebElement element1, WebElement element2) {
+        int x1 = element1.getLocation().getX();
+        int y1 = element1.getLocation().getY();
+        int width1 = element1.getSize().getWidth();
+        int height1 = element1.getSize().getHeight();
+
+        int x2 = element2.getLocation().getX();
+        int y2 = element2.getLocation().getY();
+        int width2 = element2.getSize().getWidth();
+        int height2 = element2.getSize().getHeight();
+
+        String verticalPosition;
+        if (y1 == y2) {
+            verticalPosition = "Элементы на одной горизонтальной линии";
+        } else {
+            verticalPosition = (y1 < y2) ? "Первый элемент находится выше" : "Второй элемент находится выше";
+        }
+
+        String horizontalPosition;
+        if (x1 == x2) {
+            horizontalPosition = "Элементы на одной вертикальной линии";
+        } else {
+            horizontalPosition = (x1 < x2) ? "Первый элемент находится левее" : "Второй элемент находится левее";
+        }
+
+        int area1 = width1 * height1;
+        int area2 = width2 * height2;
+        if (area1 > area2) {
+            System.out.println("Первый элемент занимает большую площадь.");
+        } else if (area1 < area2) {
+            System.out.println("Второй элемент занимает большую площадь.");
+        } else {
+            System.out.println("Площади элементов равны.");
+        }
+
+        System.out.println(verticalPosition);
+        System.out.println(horizontalPosition);
+    }
+}
---
+++ src/main/java/compare_elements/Main.java
@@ -0,0 +1,23 @@
+package compare_elements;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "D:\\05 Programming\\chromedriver-win64\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://qa-course-01.andersenlab.com/login");
+
+        WebElement element1 = driver.findElement(By.name("password"));
+        WebElement element2 = driver.findElement(By.linkText("Registration"));
+
+        ElementComparison.compareElements(element1, element2);
+
+        driver.quit();
+    }
+}
