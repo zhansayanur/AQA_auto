6408ff0f04214885e4291d4685016cde0a1adf8e
Zhansaya <zhansayanur19999@gmail.com>
2024-03-04 20:17:22 +0500

    New commit

---
+++ src/test/java/lesson_11/priority/Priority1.java
@@ -0,0 +1,46 @@
+package lesson_11.priority;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AlphabeticalOrderRunner.class)
+public class Priority1 {
+
+    @Test
+    public void a() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void b() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void c() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void d() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void e() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void f() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void g() {
+        assertTrue(true);
+    }
+
+    private void assertTrue(boolean b) {
+    }
+}
